c408ad4dcad267d9bc145398100b071541464af6
Bin Shi <i@shibin.info>
2018-01-21 18:04:24 -0800

    finished class of 01-20

---
+++ src/MongoTestApp.java
@@ -0,0 +1,25 @@
+/**
+ * Use java driver to connect to remote mongodb
+ * @author bin
+ *
+ */
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.*;
+
+public class MongoTestApp {
+    public static void main(String[] args){
+        
+//        Step 1: Establish a connection
+        MongoClient client = new MongoClient("54.218.19.24", 27107);
+        
+//        Step 2: Point a database
+        MongoDatabase database = client.getDatabase("nysedb");
+        
+//        Step 3: Point to a collection
+        MongoCollection<Document> collection = database.getCollection("stocks");
+        
+    }
+}
