4280f61019bde8c66d1996ee954c13593ca55e4f
Bin Shi <i@shibin.info>
2018-02-21 11:14:46 -0800

    WARN mapreduce.JobResourceUploader: Hadoop command-line option parsing not performed. Implement the Tool interface and execute your application with ToolRunner to remedy this.

--- src/FileInputFormatCombine.java
+++ src/FileInputFormatCombine.java
@@ -4,6 +4,7 @@
 
 import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
@@ -13,8 +14,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class FileInputFormatCombine {
+public class FileInputFormatCombine extends Configured implements Tool {
     
     private static final long MB = 1024 * 1024L;
     
@@ -52,24 +55,16 @@ public class FileInputFormatCombine {
         }
     }
 
-    /**
-     * @param args
-     */
-    public static void main (String[] args) throws Exception  {
-        Configuration conf = new Configuration();
-        
-        long maxSplitSize = MB * 128; //default value
-        if(args.length>2) {
-            maxSplitSize = MB * Long.parseLong(args[2]); // dynamic set the mapreduce.input.fileinputformat.split.maxsize
-        }
-//        conf.setLong(FileInputFormat.SPLIT_MAXSIZE, maxSplitSize);
+    @Override
+    public int run(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        Configuration conf = this.getConf();
         
         Job job = Job.getInstance(conf, "Stock Average Price");
         job.setJarByClass(FileInputFormatCombine.class);
         
         FileInputFormat.addInputPath(job, new Path(args[0]));
         job.setInputFormatClass(CombineFileInputFormat.class);
-        FileInputFormat.setMaxInputSplitSize(job, maxSplitSize); // another way to set SPLIT_MAXSIZE 
         
         job.setMapperClass(StockMapper.class);
         job.setReducerClass(StockReducer.class);
@@ -78,6 +73,22 @@ public class FileInputFormatCombine {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(DoubleWritable.class);
         
-        System.exit(job.waitForCompletion(true)?0:1);
+        return job.waitForCompletion(true)?0:1;
+    }
+    
+    /**
+     * Implementing the Tool interface for MapReduce driver:
+     * hadoop jar file.jar MainClass -D mapreduce.input.fileinputformat.split.maxsize=128 /input /output
+     * @param args
+     */
+    public static void main (String[] args)  {
+
+        int code = -1;
+        try {
+            code = ToolRunner.run(new Configuration(), new FileInputFormatCombine(), args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.exit(code);
     }
 }
